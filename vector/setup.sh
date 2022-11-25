b90f19a2c050d8eb8965b160e6ca52ec2096ed98
anjmao <andzej.maciusovic@gmail.com>
2022-11-25 16:54:09 +0200

    Add loki

--- vector/setup.sh
+++ vector/setup.sh
@@ -4,6 +4,8 @@ set -e
 
 kubectl create ns poc
 kubectl apply -f tetra-vector-config.yaml -n poc
+helm upgrade -i grafana grafana/grafana -n poc -f ./grafana.yaml
+helm upgrade -i loki grafana/loki-simple-scalable -n poc -f ./loki.yaml
 helm upgrade -i tetragon cilium/tetragon -n poc -f ./tetragon.yaml
 helm upgrade -i vector vector/vector -n poc -f ./vector.yaml
 helm upgrade -i metrics vm/victoria-metrics-single -n poc -f ./victoria-metrics-values.yaml
